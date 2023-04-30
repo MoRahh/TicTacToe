0071b2b100c9508cf67cf14da1f513040ddba988
MoRahh <moodyaburahma@gmail.com>
2023-04-30 09:53:33 +0300

    Added some Logic

--- tictactoe.py
+++ tictactoe.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from constants import *
+import numpy as np
 
 # PyGame
 pygame.init()
@@ -8,8 +9,21 @@ screen = pygame.display.set_mode( (WIDTH, HEIGHT) )
 pygame.display.set_caption('TICTACTOE-AI')
 screen.fill(BG_COLOR)
 
+class Board:
+    def __init__(self):
+        self.squares = np.zeros((ROWS, COLS))
+
+    def mark_sqr(self, row, col, player):
+        self.squares[row][col] = player
+
+    def empty_sqr(self, row, col):
+        return self.squares[row][col] == 0
+
+
 class Game:
     def __init__(self):
+        self.board = Board()
+        self.player = 1
         self.show_lines() 
 
     def show_lines(self):
@@ -20,11 +34,16 @@ class Game:
         pygame.draw.line(screen, LINE_COLOR, (0 , SQ_SIZE), (WIDTH , SQ_SIZE), LINE_WIDTH)
         pygame.draw.line(screen, LINE_COLOR, (0, HEIGHT - SQ_SIZE), (WIDTH, HEIGHT - SQ_SIZE), LINE_WIDTH)
 
+    def next_turn(self):
+        self.player = self.player % 2 + 1
+
 
 def main():
 
     # Game Object
     game = Game()
+    board = game.board
+
 
     
     while True:
@@ -32,6 +51,15 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = event.pos
+                row = pos[1] // SQ_SIZE
+                col = pos[0] // SQ_SIZE
+
+                if board.empty_sqr(row, col):
+                    board.mark_sqr(row, col, game.player)
+                    game.next_turn()
 
         pygame.display.update()
 
